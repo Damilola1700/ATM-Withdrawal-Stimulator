6c684384b406742310294a1b40d434da10097f81
loveth <lovethdamilola206@gmail.com>
2026-05-09 17:35:29 +0100

    atm withdrawal

--- atm-withdrawal.js
+++ atm-withdrawal.js
@@ -3,24 +3,22 @@ let WithdrawAmount = document.getElementById("WriteAmount");
 let output = document.getElementById("messageDisplay");
 
 function withdrawHere() {
-  if (WriteAmount.value === "") {
+  if (WithdrawAmount.value === "") {
     output.innerHTML = " Kindly input your amount";
     output.classList.remove("hidden");
     return;
-    
   }
 
-  if (WithdrawAmount.value <= balance){
-    output.innerHTML = `Withdrawal Successful! Your available balance is ${balance - WithdrawAmount.value}`;
+  if (WithdrawAmount.value <= balance) {
+    balance -= WithdrawAmount.value;
+    output.innerHTML = `Withdrawal Successful! Your available balance is ${balance}`;
     output.classList.remove("hidden");
-  } else{
-    output.innerHTML = "Insufficient Funds"
+  } else {
+    output.innerHTML = "Insufficient Funds";
   }
 
-  if (WithdrawAmount.value < 1000 && balance >0 ){
+  if (WithdrawAmount.value < 1000 && balance > 0) {
     output.innerHTML = "Minimum withdrawal is 1000";
     output.classList.remove("hidden");
   }
 }
-
-
