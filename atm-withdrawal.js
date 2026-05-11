de0e2606d05fce58eab7e2eef227b2b03eab3b31
loveth <lovethdamilola206@gmail.com>
2026-05-11 22:52:24 +0100

    atm withdrawal stimulator

--- atm-withdrawal.js
+++ atm-withdrawal.js
@@ -2,23 +2,42 @@ let balance = 10000;
 let WithdrawAmount = document.getElementById("WriteAmount");
 let output = document.getElementById("messageDisplay");
 
-function withdrawHere() {
+ function withdrawHere() {
   if (WithdrawAmount.value === "") {
-    output.innerHTML = " Kindly input your amount";
-    output.classList.remove("hidden");
-    return;
-  }
 
-  if (WithdrawAmount.value <= balance) {
+    output.innerHTML = " Kindly input your amount";
+    
+  } else if (isNaN(WithdrawAmount.value)) {
+
+    output.innerHTML = "Input amount in number";
+   
+  } else if (WithdrawAmount <= 0 ) {
+
+    output.innerHTML = "Input a valid number"
+
+  } else if ( WithdrawAmount.value < 1000 ) {
+
+   
+    output.innerHTML = "Minimum withdrawal is 1000" ;
+    
+  } else if (WithdrawAmount.value <= balance) {
+
     balance -= WithdrawAmount.value;
     output.innerHTML = `Withdrawal Successful! Your available balance is ${balance}`;
-    output.classList.remove("hidden");
+   
   } else {
+
     output.innerHTML = "Insufficient Funds";
+    
   }
 
-  if (WithdrawAmount.value < 1000 && balance > 0) {
-    output.innerHTML = "Minimum withdrawal is 1000";
     output.classList.remove("hidden");
-  }
+
 }
+
+
+
+
+
+
+
